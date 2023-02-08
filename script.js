4a8ea905a58156b9ac035e99d162dd83285cb852
alan <alan@iMac-de-alan.local>
2023-02-07 22:03:49 -0600

    Add function to toggle the readStatus to true or false, change its style and the textcontent of the button

--- script.js
+++ script.js
@@ -29,16 +29,9 @@ let addBtn = document.getElementById('addBook');
 let inputs = document.querySelectorAll('input')
 
 
-
-
-
-
+//Each time a form is submitted the input goes to the book constructor and to the myLibrary array
+// the elements on the array create DOM elements so each book object looks like a card
 submitBtn.addEventListener('click', submitClick);
-addBtn.addEventListener('click', openForm);
-
-
-
-
 
 function submitClick(event){ 
     let newTitle = titleInput.value;
@@ -60,43 +53,62 @@ function submitClick(event){
 
 
 function createCards(){ 
-    myLibrary.forEach(book =>{
-        let cardsContainer = createElementWithClass('div', 'cardsContainer');
-        let cardAuthor = createElementWithClass('div', 'cardAuthor');
-        let cardTitle = createElementWithClass('div', 'cardTitle');
-        let cardPages = createElementWithClass('div', 'cardPages');
-        let statusBtn = createElementWithClass('button', 'statusBtn');
-        let deleteBtn = createElementWithClass('button', 'deleteBtn');
-        container.appendChild(cardsContainer);
-        cardsContainer.appendChild(cardAuthor);
-        cardsContainer.appendChild(cardTitle);
-        cardsContainer.appendChild(cardPages);
-        cardsContainer.appendChild(statusBtn);
-        cardsContainer.appendChild(deleteBtn);
-        cardAuthor.textContent = `Author: ${book.author}`;
-        cardTitle.textContent = `Title: ${book.title}`;
-        cardPages.textContent = `Number of pages: ${book.pages}`;
-        book.readStatus ? statusBtn.textContent = "Done reading it" : statusBtn.textContent = "I'll read it later";
-        book.readStatus ? statusBtn.classList.add('trueBtn') : statusBtn.classList.add('falseBtn');
-        deleteBtn.textContent = 'Delete';
-    })
-}
+  
+};
+
+
+myLibrary.forEach(book =>{
+
+    let cardsContainer = createElementWithClass('div', 'cardsContainer');
+    let cardAuthor = createElementWithClass('div', 'cardAuthor');
+    let cardTitle = createElementWithClass('div', 'cardTitle');
+    let cardPages = createElementWithClass('div', 'cardPages');
+    let statusBtn = createElementWithClass('button', 'statusBtn');
+    let deleteBtn = createElementWithClass('button', 'deleteBtn');
+
+    container.appendChild(cardsContainer);
+    cardsContainer.appendChild(cardAuthor);
+    cardsContainer.appendChild(cardTitle);
+    cardsContainer.appendChild(cardPages);
+    cardsContainer.appendChild(statusBtn);
+    cardsContainer.appendChild(deleteBtn);
+
+    cardAuthor.textContent = `Author: ${book.author}`;
+    cardTitle.textContent = `Title: ${book.title}`;
+    cardPages.textContent = `Number of pages: ${book.pages}`;
+    book.readStatus ? statusBtn.textContent = "Done reading it" : statusBtn.textContent = "I'll read it later";
+    book.readStatus ? statusBtn.classList.add('trueBtn') : statusBtn.classList.add('falseBtn');
+    deleteBtn.textContent = 'Delete';
+    
+    statusBtn.addEventListener('click', function() {
+        book.readStatus = !book.readStatus;
+        statusBtn.classList.toggle('falseBtn');
+        statusBtn.classList.toggle('trueBtn');
+        statusBtn.textContent = book.readStatus ? statusBtn.textContent = "Done reading it" : statusBtn.textContent = "I'll read it later";
+      });
+
+    deleteBtn.addEventListener('click', function() {
+        console.log('hola');
+      });
+})
 
 function createElementWithClass (elementType, className){
     let element = document.createElement(elementType);
     element.classList.add(className);
     return element
-}
-
+};
 
 function addBookToLibrary(book){ 
     myLibrary.push(book)
 };
-
-
 function clearForm (){
     inputs.forEach(input => input.value ='');
 };
+
+
+//pop up form 
+addBtn.addEventListener('click', openForm);
+
 function openForm(){
     document.getElementById("myForm").style.display = "block";
 };
@@ -105,14 +117,6 @@ function closeForm(){
 };
 
 
-
-
-
-
-
-// Book.prototype.info = function() {
-//     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus ? 'already read' : 'not read yet'}`
-// }
 
 
 
