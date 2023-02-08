60e0e0cbe74fe38ea0d3779e89139506b4e1b210
alan <alan@iMac-de-alan.local>
2023-02-07 21:39:33 -0600

    Add function that clears forms input, create function that creates element and class in one line, added style, the readStatus button checks for true or false to give one class or another

--- script.js
+++ script.js
@@ -1,25 +1,36 @@
+function Book(title, author, pages, readStatus){
+    this.title = title
+    this.author = author
+    this.pages = pages
+    this.readStatus = readStatus
+};
+
+let myLibrary = [];
+
+// book examples
+const theHobbit = new Book('The Hobbit', 'J.R.R Tolkein', '295', true);
+const berserk = new Book('Berserk', 'Kentaro Miura', '9500', true);
+const tao = new Book ('Tao', 'Alan Watts', '131', false);
+addBookToLibrary(berserk);
+addBookToLibrary(theHobbit);
+addBookToLibrary(tao);
+
+
+
+//DOM elements
+const container = document.getElementById('container');
+
 let titleInput = document.getElementById('title');
 let authorInput = document.getElementById('author');
 let pagesInput = document.getElementById('pages');
-let trueInput = document.getElementById('true');
-let falseInput = document.getElementById('false');
+let checkbox = document.getElementById('readStatus');
 let submitBtn = document.getElementById('submit');
 let addBtn = document.getElementById('addBook');
-const container = document.getElementById('container');
-
-const theHobbit = new Book('The Hobbit', 'J.R.R Tolkein', '295', true);
-const berserk = new Book('Berserk', 'Kentaro Miura', '9500', true);
-const tao = new Book ('Tao', 'Alan Watts', '131');
-let myLibrary = [];
+let inputs = document.querySelectorAll('input')
 
 
 
-addBookToLibrary(berserk);
-addBookToLibrary(theHobbit);
-addBookToLibrary(tao);
-addBookToLibrary(berserk);
-addBookToLibrary(theHobbit);
-addBookToLibrary(tao);
+
 
 
 submitBtn.addEventListener('click', submitClick);
@@ -33,46 +44,48 @@ function submitClick(event){
     let newTitle = titleInput.value;
     let newAuthor = authorInput.value;
     let newPages = pagesInput.value;
+    let readStatus = checkbox.checked ? true : false;
     if (newTitle === '' || newAuthor === '' || newPages === ''){  
         return
     } else { 
     container.innerHTML = "";
-    const bookForm = new Book(newTitle, newAuthor, newPages); 
+    const bookForm = new Book(newTitle, newAuthor, newPages, readStatus); 
     addBookToLibrary(bookForm); 
     createCards();
+    clearForm();
     event.preventDefault();
     closeForm();
     };
 };
 
-myLibrary.forEach(book =>{
-    let cardsContainer = document.createElement('div');
-    let cardAuthor = document.createElement('div');
-    let cardTitle = document.createElement('div');
-    let cardPages = document.createElement('div');
-    let statusBtn = document.createElement('button');
-    let deleteBtn = document.createElement('button');
-    container.appendChild(cardsContainer);
-    cardsContainer.appendChild(cardAuthor);
-    cardsContainer.appendChild(cardTitle);
-    cardsContainer.appendChild(cardPages);
-    cardsContainer.appendChild(statusBtn);
-    cardsContainer.appendChild(deleteBtn);
-    cardsContainer.classList.add('cardsContainer');
-    cardAuthor.classList.add('author');
-    cardTitle.classList.add('title');
-    cardPages.classList.add('pages');
-    statusBtn.classList.add('statusBtn');
-    deleteBtn.classList.add('deleteBtn');
-    cardAuthor.textContent = `Author: ${book.author}`;
-    cardTitle.textContent = `Title: ${book.title}`;
-    cardPages.textContent = `Number of pages: ${book.pages}`;
-    statusBtn.textContent = 'Read status';
-    deleteBtn.textContent = 'Delete';
-})
 
 function createCards(){ 
-    
+    myLibrary.forEach(book =>{
+        let cardsContainer = createElementWithClass('div', 'cardsContainer');
+        let cardAuthor = createElementWithClass('div', 'cardAuthor');
+        let cardTitle = createElementWithClass('div', 'cardTitle');
+        let cardPages = createElementWithClass('div', 'cardPages');
+        let statusBtn = createElementWithClass('button', 'statusBtn');
+        let deleteBtn = createElementWithClass('button', 'deleteBtn');
+        container.appendChild(cardsContainer);
+        cardsContainer.appendChild(cardAuthor);
+        cardsContainer.appendChild(cardTitle);
+        cardsContainer.appendChild(cardPages);
+        cardsContainer.appendChild(statusBtn);
+        cardsContainer.appendChild(deleteBtn);
+        cardAuthor.textContent = `Author: ${book.author}`;
+        cardTitle.textContent = `Title: ${book.title}`;
+        cardPages.textContent = `Number of pages: ${book.pages}`;
+        book.readStatus ? statusBtn.textContent = "Done reading it" : statusBtn.textContent = "I'll read it later";
+        book.readStatus ? statusBtn.classList.add('trueBtn') : statusBtn.classList.add('falseBtn');
+        deleteBtn.textContent = 'Delete';
+    })
+}
+
+function createElementWithClass (elementType, className){
+    let element = document.createElement(elementType);
+    element.classList.add(className);
+    return element
 }
 
 
@@ -80,6 +93,10 @@ function addBookToLibrary(book){
     myLibrary.push(book)
 };
 
+
+function clearForm (){
+    inputs.forEach(input => input.value ='');
+};
 function openForm(){
     document.getElementById("myForm").style.display = "block";
 };
@@ -91,20 +108,13 @@ function closeForm(){
 
 
 
-function Book(title, author, pages){
-    this.title = title
-    this.author = author
-    this.pages = pages
-};
 
 
-// readStatus
-// this.readStatus = readStatus
 // Book.prototype.info = function() {
 //     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.readStatus ? 'already read' : 'not read yet'}`
 // }
 
 
 
-//boton para eliminar del array
+
  
